2c6ba7e60953d72e466731f7ca3092e62a2a7b9c
SupSonic <clementbrieuc@gmail.com>
2018-07-05 12:39:41 +0200

    modif classes Carte et Valeur

--- src/com/brieucclement/tp18bis/Carte.java
+++ src/com/brieucclement/tp18bis/Carte.java
@@ -2,5 +2,6 @@ package com.brieucclement.tp18bis;
 
 public class Carte {
 	
+	float nbCartes = 32f;
 	
 }
--- src/com/brieucclement/tp18bis/Valeur.java
+++ src/com/brieucclement/tp18bis/Valeur.java
@@ -1,14 +1,18 @@
 package com.brieucclement.tp18bis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Valeur {
 
-	 List<String> list = new ArrayList<String>();
-	      list.add("7");
-	      list.add("8");
-	      list.add("9");
-	      list.add("10");
-	      list.add("Valet");
-	      list.add("Dame");
-	      list.add("Roi");
-	      list.add("As");
+	 List<String> valeur = new ArrayList<String>(); {
+	      valeur.add("7");
+	      valeur.add("8");
+	      valeur.add("9");
+	      valeur.add("10");
+	      valeur.add("Valet");
+	      valeur.add("Dame");
+	      valeur.add("Roi");
+	      valeur.add("As");
+ 	}
 }
